7313616195fc5a01b8374960496810896b48a951
Nour Moftah <noursaad2213@gmail.com>
2026-06-05 19:35:35 +0300

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,29 @@
 import express from "express";
-import { ENV } from "./lib/env.js"
+import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
 
+const __dirname = path.resolve();
 
-console.log(ENV.PORT)
+console.log(ENV.PORT);
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({ msg: "you did it" });
 });
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "box endpoint" });
+});
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+console.log(__dirname);
+console.log(path.join(__dirname, "../frontend/dist"));
 
 app.listen(ENV.PORT, () => {
   console.log(`server is running on port ${ENV.PORT}`);
